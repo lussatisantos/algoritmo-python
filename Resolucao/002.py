12c47a028c4348d98a6d32817064b27a9f782306
Lussati Santos <lussatisantos10@gmail.com>
2023-07-05 20:06:15 +0100

    Create 002.py

---
+++ Resolucao/002.py
@@ -0,0 +1,5 @@
+print('Soma de dois valores')
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite outro numero: '))
+soma = num1 + num2
+print('A soma de {} e {} sera igual a {}'.format(num1, num2, soma))
