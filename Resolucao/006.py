660d775de5c72478a3ba56b5adcb1c886e236232
Lussati Santos <lussatisantos10@gmail.com>
2023-07-06 13:12:23 +0100

    Create 006.py

---
+++ Resolucao/006.py
@@ -0,0 +1,3 @@
+nota1 = float(input('Digite a nota do primeiro aluno: '))
+nota2 = float(input('Digite a nota do segundo aluno: '))
+print('A media entre das notas do primeiro aluno e o segundo sera de {}' .format((nota1 + nota2)/2))
