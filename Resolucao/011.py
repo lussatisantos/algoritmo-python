87b36fa89a736ce5608df9903038bb521cf6eaa9
Lussati Santos <lussatisantos10@gmail.com>
2023-07-06 14:17:16 +0100

    Update 011.py

--- Resolucao/011.py
+++ Resolucao/011.py
@@ -1,3 +1,3 @@
 preco = float(input('Digite o preco de um produto: '))
 
-print('O produto tem o preco original de {}, como estamos em promocao, voce teras 5% de desconto e pagaras somente {} AOA' .format(preco, (preco*0.05)- preco))
+print('O produto tem o preco original de {}, como estamos em promocao, voce teras 5% de desconto e pagaras somente {:.2f} AOA' .format(preco, (preco*0.05)- preco))
