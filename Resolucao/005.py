839de760cdb5c6c2d9625c8d3d5a581f24d824c5
Lussati Santos <lussatisantos10@gmail.com>
2023-07-06 13:06:24 +0100

    Update 005.py

---
+++ Resolucao/005.py
@@ -0,0 +1,4 @@
+num = int(input('Digite um numero: '))
+print('O dobro de {} sera igual a {}' .format(num, num*2))
+print('O triplo de {} sera igual a {}' .format(num, num*3))
+print('A raiz quadrada de {} sera igual a {}' .format(num, num**(1/2)))
