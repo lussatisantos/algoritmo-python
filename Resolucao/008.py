e5cd816108d9cfdab28f593d35b249bc52826409
Lussati Santos <lussatisantos10@gmail.com>
2023-07-06 13:38:49 +0100

    Create 008.py

---
+++ Resolucao/008.py
@@ -0,0 +1,19 @@
+num = int(input('Digite um numero para fazer a sua tabuada: '))
+
+if num == 0:
+    print('A tabuada de 0 sera 0 em todos numeros que multiplicarem por ele')
+else:
+    print('=============')
+    print('{} x 1 = {}' .format(num, num))
+    print('{} x 2 = {}' .format(num, num*2))
+    print('{} x 3 = {}' .format(num, num*3))
+    print('{} x 4 = {}' .format(num, num*4))
+    print('{} x 5 = {}' .format(num, num*5))
+    print('{} x 6 = {}' .format(num, num*6))
+    print('{} x 7 = {}' .format(num, num*7))
+    print('{} x 8 = {}' .format(num, num*8))
+    print('{} x 9 = {}' .format(num, num*9))
+    print('{} x 10 = {}' .format(num, num*10))
+    print('{} x 11 = {}' .format(num, num*11))
+    print('{} x 12 = {}' .format(num, num*12))
+    print('============')
