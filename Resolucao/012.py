102f134d1edad2726f6b2dcdf269a0ab9647774d
Lussati Santos <lussatisantos10@gmail.com>
2023-07-06 14:22:59 +0100

    Update 012.py

--- Resolucao/012.py
+++ Resolucao/012.py
@@ -1,3 +1,3 @@
 salario = float(input('Digite o seu salario: '))
 
-print('Voce ganha {} Kz, com 15% de aumento voce recebera neste mes {} Kzs' .format(salario, (salario*0.15)+salario))
+print('Voce ganha {} Kz, com 15% de aumento voce recebera neste mes {:.2f} Kzs' .format(salario, (salario*0.15)+salario))
