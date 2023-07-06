a6642d8d2344bdda42b8b2db9681290715de81ae
Lussati Santos <lussatisantos10@gmail.com>
2023-07-06 13:28:17 +0100

    Update 007.py

--- Resolucao/007.py
+++ Resolucao/007.py
@@ -2,3 +2,8 @@ metro = float(input('Digite a quantidade de metros: '))
 
 print('{}metros equivale a: ')
 print('{}km' .format(metro/1000))
+print('{}hm' .format(metro/100))
+print('{}dm' .format(metro/10))
+print('{}dc '.format(metro*10))
+print('{}cm '.format(metro*100))
+print('{:.0f}mm '.format(metro*1000))
