66a1e7f27255208f8ebd8b298d05df02c3f32890
Lussati Santos <lussatisantos10@gmail.com>
2023-07-08 10:54:49 +0100

    Create 019.py

---
+++ Resolucao/019.py
@@ -0,0 +1,11 @@
+import random
+
+aluno1 = input('Digite o nome do primeiro aluno: ')
+aluno2 = input('Digite o nome do segundo aluno: ')
+aluno3 = input('Digite o nome do terceiro aluno: ')
+aluno4 = input('Digite o nome do quarto aluno: ')
+
+sorteio = [aluno1, aluno2, aluno3, aluno4]
+
+print('A ordem de apresentacao do trabalho sera ')
+print(sorteio)
