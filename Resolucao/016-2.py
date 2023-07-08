c2874fab91f52b05873d6ae86de59f07fb23cde4
Lussati Santos <lussatisantos10@gmail.com>
2023-07-08 10:12:32 +0100

    Update 016-2.py

--- Resolucao/016-2.py
+++ Resolucao/016-2.py
@@ -4,4 +4,4 @@ co = float(input('Comprimento do cateto oposto: '))
 ca = float(input('Comprimento do cateto adjacente: '))
 
 hi = math.hypot(co, ca)
-print('A hipotenusa vai medir {}' .format(hi))
+print('A hipotenusa vai medir {:.2f}' .format(hi))
