e884ab86e555b6f90930137635b9afc82c0d2e21
Lussati Santos <lussatisantos10@gmail.com>
2023-07-08 10:28:47 +0100

    tangente feita

--- Resolucao/017.py
+++ Resolucao/017.py
@@ -1,9 +1,12 @@
 import math
 
-angulo = float(input('Digite o angulo  que voce deseja: '))
+angulo = float(input('Digite o angulo que voce deseja: '))
 
 seno = math.sin(math.radians(angulo))
 print('O angulo de {} tem o SENO de {:.2f}' .format(angulo, seno))
 
 cosseno = math.cos(math.radians(angulo))
 print('O angulo de {} tem o COSSENO de {:.2f}' .format(angulo, cosseno))
+
+tangente = math.tan(math.radians(angulo))
+print('O angulo de {} tem a TANGENTE de {:.2f}' .format(angulo, tangente))
