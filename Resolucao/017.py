e260a1ce509b0595602e9eb6bef444852a07dab8
Lussati Santos <lussatisantos10@gmail.com>
2023-07-08 10:26:56 +0100

    cosseno feito

--- Resolucao/017.py
+++ Resolucao/017.py
@@ -5,3 +5,5 @@ angulo = float(input('Digite o angulo  que voce deseja: '))
 seno = math.sin(math.radians(angulo))
 print('O angulo de {} tem o SENO de {:.2f}' .format(angulo, seno))
 
+cosseno = math.cos(math.radians(angulo))
+print('O angulo de {} tem o COSSENO de {:.2f}' .format(angulo, cosseno))
