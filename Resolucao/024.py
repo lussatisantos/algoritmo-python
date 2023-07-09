fb0ba9030fa0c5020adb0f62136f1d53146d4fd7
Lussati Santos <lussatisantos10@gmail.com>
2023-07-09 20:58:47 +0100

    finished

--- Resolucao/024.py
+++ Resolucao/024.py
@@ -1,4 +1,4 @@
-nom = input('Digite algo: ').upper()
+nom = input('Digite algo: ').upper().strip()
 
 print('A letra A aparece {} vezes na frase.' .format(nom.count('A')))
 print('A primeira letra A apareceu na posicao {}' .format(nom.find('A')+1))
