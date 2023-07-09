faa9eeccfff8782c85ea8fd4ee32da0ad0db91ff
Lussati Santos <lussatisantos10@gmail.com>
2023-07-09 20:14:35 +0100

    space inuteis eliminados

--- Resolucao/020.py
+++ Resolucao/020.py
@@ -2,4 +2,4 @@ nome = input('Digite o seu nome: ').strip()
 
 print('O seu nome em maisculas sera {}' .format(nome.upper()))
 print('O seu nome em minusculas sera {}' .format(nome.lower()))
-print ('O seu nome tem {} letras' .format(len(nome)))
+print ('O seu nome tem {} letras' .format(len(nome) - nome.count(' ')))
