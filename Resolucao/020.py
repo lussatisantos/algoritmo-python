c298564890284b43d7c9a13dd685209ed0936ea3
Lussati Santos <lussatisantos10@gmail.com>
2023-07-09 20:10:39 +0100

    space

--- Resolucao/020.py
+++ Resolucao/020.py
@@ -2,3 +2,4 @@ nome = input('Digite o seu nome: ')
 
 print('O seu nome em maisculas sera {}' .format(nome.upper()))
 print('O seu nome em minusculas sera {}' .format(nome.lower()))
+print ('O seu nome tem {} letras' .format(len(nome)))
