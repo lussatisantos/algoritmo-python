67090951995a914cecb1ac108172e62a93a22b08
Lussati Santos <lussatisantos10@gmail.com>
2023-07-09 20:58:08 +0100

    ultima letra A

--- Resolucao/024.py
+++ Resolucao/024.py
@@ -2,3 +2,4 @@ nom = input('Digite algo: ').upper()
 
 print('A letra A aparece {} vezes na frase.' .format(nom.count('A')))
 print('A primeira letra A apareceu na posicao {}' .format(nom.find('A')+1))
+print('A ultima letra A apareceu na posicao {}' .format(nom.rfind('A')+1))
