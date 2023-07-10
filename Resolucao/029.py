df8bc634e5d70c41e82397b0bac3da099f6ca291
Lussati Santos <lussatisantos10@gmail.com>
2023-07-10 12:39:57 +0100

    Update 029.py

--- Resolucao/029.py
+++ Resolucao/029.py
@@ -1,8 +1,10 @@
 viagem = float(input('Digite a distancia da viagem: '))
 
+print('Voce esta prestes a comecar uma viagem de {} Km' .format(viagem))
+
 if viagem<=200:
-    preco = viagem * 0.50
-    print('Voce pagara {} pela viagem' .format(preco))
+    preco = viagem * 200
+    print('Voce pagara {} Kz pela viagem' .format(preco))
 else:
-    preco = viagem * 0.45
-    print('Voce pagara {} pela viagem' .format(preco))
+    preco = viagem * 175
+    print('Voce pagara {} Kz pela viagem' .format(preco))
