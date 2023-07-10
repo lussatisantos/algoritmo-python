1580f7d29cbd64543c3a5106709600bac819024c
Lussati Santos <lussatisantos10@gmail.com>
2023-07-10 12:24:43 +0100

    Update 027.py

---
+++ Resolucao/027.py
@@ -0,0 +1,8 @@
+velocidade = float(input('Digite a velocidade do carro em Km: '))
+
+if velocidade<=80:
+    print('Tenha um bom dia, e dirija com cuidado:')
+else:
+    multa = velocidade - 80
+    print('Voce foi multado por excesso de velocidade')
+    print('A sua multa esta avaliada em {} AOA' .format(multa*7))
