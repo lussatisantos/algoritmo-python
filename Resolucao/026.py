0a57b46dedcda24132651878cfc177e2421cfe55
Lussati Santos <lussatisantos10@gmail.com>
2023-07-10 12:13:26 +0100

    finneshed

--- Resolucao/026.py
+++ Resolucao/026.py
@@ -5,4 +5,10 @@ num = int(input('Eu vou pensar um numero de 0 a 5, adivinhe: '))
 
 adivinha = random.randint(0, 5)
 
-print(adivinha)
+if num>5:
+    print('Numero invalido, tente de 0 a 5')
+else:
+    if adivinha == num:
+        print('PARABENS, voce ganhou o jogo, eu pensei em!!!!')
+    else:
+        print('Voce perdeu, tente novamente!!!')
