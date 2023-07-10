a4fb623dadcf0794dcfad5e964405859036d8976
Lussati Santos <lussatisantos10@gmail.com>
2023-07-10 12:51:06 +0100

    finneshed

--- Resolucao/030.py
+++ Resolucao/030.py
@@ -1,6 +1,10 @@
-ano = int(input('Qual ano queres analisar? '))
+from datetime import date
 
-if ano % 4 == 0:
+ano = int(input('Qual ano queres analisar? Digite 0 para ano actual: '))
+
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('O ano {} e BISSEXTO' .format(ano))
 else:
     print('O ano {} nao e bissexto' .format(ano))
