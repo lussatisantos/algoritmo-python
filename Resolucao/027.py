c88fcc25acc66fb9c222cbf0d8b9cfd21b33b471
Lussati Santos <lussatisantos10@gmail.com>
2023-07-10 12:26:24 +0100

    Update 027.py

--- Resolucao/027.py
+++ Resolucao/027.py
@@ -4,5 +4,5 @@ if velocidade<=80:
     print('Tenha um bom dia, e dirija com cuidado:')
 else:
     multa = velocidade - 80
-    print('Voce foi multado por excesso de velocidade')
+    print('Voce foi multado por excesso de velocidade permitida de 80km/h')
     print('A sua multa esta avaliada em {} AOA' .format(multa*140))
