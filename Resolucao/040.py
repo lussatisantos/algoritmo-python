c8f18553814cd842295e6b4063154c450175f94c
Lussati Santos <lussatisantos10@gmail.com>
2023-07-11 14:17:21 +0100

    finished

--- Resolucao/040.py
+++ Resolucao/040.py
@@ -6,5 +6,9 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Os segmentos acima pode formar triangulo ', end='')
     if r1 == r2 == r3:
         print('EQUILATERO')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO')
+    else:
+        print('ISOSCELES')
 else:
     print('Os segmentos acima NAO PODEM FORMAR TRIANGULO')
