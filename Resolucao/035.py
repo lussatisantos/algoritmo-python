568fd1413b1d0a1809094eba5dab0f9b1e8259fc
Lussati Santos <lussatisantos10@gmail.com>
2023-07-11 13:07:22 +0100

    binario

--- Resolucao/035.py
+++ Resolucao/035.py
@@ -7,3 +7,6 @@ print(''' Escolha uma das opcoes para converter:
 ''')
       
 convert = int(input('Digite a sua opcao: '))
+
+if convert == 1:
+    print('{} convertido em BINARIO sera {}' .format(num, bin(num)))
