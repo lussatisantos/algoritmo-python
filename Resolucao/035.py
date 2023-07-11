ef76ed3907614d95fd2a4c9023d1481f9342002f
Lussati Santos <lussatisantos10@gmail.com>
2023-07-11 13:03:54 +0100

    Create 035.py

---
+++ Resolucao/035.py
@@ -0,0 +1,9 @@
+num = int(input('Digite um numero: '))
+
+print(''' Escolha uma das opcoes para converter:
+[1] converta em BINARIO
+[2] converta em OCTAL
+[3] converta em HEXADECIMAL
+''')
+      
+convert = int(input('Digite a sua opcao: '))
