feb8ce560ffd46b867f48814cdb1a25c74305d2a
Lussati Santos <lussatisantos10@gmail.com>
2023-07-11 14:13:45 +0100

    Update 040.py

--- Resolucao/040.py
+++ Resolucao/040.py
@@ -3,8 +3,8 @@ r2 = float(input('Segundo segmento: '))
 r3 = float(input('Terceiro segmento: '))
 
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('Os segmentos acima PODEM FORMAR TRIANGULO')
+    print('Os segmentos acima pode formar triangulo ', end='')
     if r1 == r2 == r3:
-        print('Equilatero')
+        print('EQUILATERO')
 else:
     print('Os segmentos acima NAO PODEM FORMAR TRIANGULO')
