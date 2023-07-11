3ee96ff62a1bdd27d6241fb2d9a6ae8c18473fbb
Lussati Santos <lussatisantos10@gmail.com>
2023-07-11 13:17:50 +0100

    finished

--- Resolucao/036.py
+++ Resolucao/036.py
@@ -4,4 +4,6 @@ num2 = int(input('Digite outro numero: '))
 if num1 > num2:
     print('Primeiro numero e MAIOR')
 elif num2 > num1:
-    print()
+    print('O segundo numero e MAIOR')
+else:
+    print('Nao existe maior, eles SAO IGUAIS')
