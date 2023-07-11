05299b1830275bf5d2f0f6a2a844390bc337986c
Lussati Santos <lussatisantos10@gmail.com>
2023-07-11 14:03:39 +0100

    Update 039.py

--- Resolucao/039.py
+++ Resolucao/039.py
@@ -1,4 +1,13 @@
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 
-media = (nota1 + nota2) /2
+media = (nota1 + nota2) / 2
+
+if media < 5:
+    print('Infelizmente voce esta REPROVADO')
+elif media <= 6.9:
+    print('Voce foi ao RECURSO')
+elif media>= 7 and media < 10.1:
+    print('PARABENS, voce esta APROVADO')
+else:
+    print('ERRO, tente novamente!!')
