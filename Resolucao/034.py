65ef04b0250a1697c1466192b22234b5a61c305b
Lussati Santos <lussatisantos10@gmail.com>
2023-07-11 12:42:01 +0100

    testando valor a pagar

--- Resolucao/034.py
+++ Resolucao/034.py
@@ -1,3 +1,7 @@
 casa = float(input('Quanto custa a casa? AOA '))
 salario = float(input('Qual o teu salario mensal? AOA'))
 anos = int(input('Em quantos anos de prestacao? '))
+
+valor_a_pagar = casa / (anos * 12)
+
+print(valor_a_pagar)
