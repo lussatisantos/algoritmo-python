00edb6e37cf9eae64bc1a1c430ccb69e3207a16b
Lussati Santos <lussatisantos10@gmail.com>
2023-07-12 23:15:31 +0100

    formatar os decimais

--- Resolucao/041.py
+++ Resolucao/041.py
@@ -4,18 +4,18 @@ altura = float(input('Digite a sua altura: '))
 imc = peso / (altura ** 2)
 
 if imc < 18.5:
-    print('O seu IMC e {}' .format(imc))
+    print('O seu IMC e {:.2f}' .format(imc))
     print('Voce esta ABAIXO DO PESO')
 elif imc <= 25:
-    print('O seu IMC e {}' .format(imc))
+    print('O seu IMC e {:.2f}' .format(imc))
     print('PARABENS, voce tem o PESO IDEAL')
 elif imc <= 30:
-    print('O seu IMC e {}' .format(imc))
+    print('O seu IMC e {:.2f}' .format(imc))
     print('Voce esta com SOBREPESO, aproveite fazer EXERCICIOS')
 elif imc <= 40:
-    print('O seu IMC e {}' .format(imc))
+    print('O seu IMC e {:.2f}' .format(imc))
     print('Voce tem OBESIDADE, Cuidado!!!')
     print('Contacte um Personal Trainer ou va a um ginasio')
 else:
-    print('O seu IMC e {}' .format(imc))
+    print('O seu IMC e {:.2f}' .format(imc))
     print('Voce tem OBESIDADE MORBIDA, contacte um Medico para uma assistencia')
