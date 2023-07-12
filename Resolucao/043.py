6cd99803c12a9544f1f8afbdbe9639fe552f2ac0
Lussati Santos <lussatisantos10@gmail.com>
2023-07-13 00:08:32 +0100

    finished

--- Resolucao/043.py
+++ Resolucao/043.py
@@ -9,5 +9,35 @@ print(''' Suas opcoes:
 jogador = int(input('Qual sera a sua jogada: '))
 computador = randint(0, 2)
 
+print('-=' * 11)
 print('O computador escolheu {}' .format(itens[computador]))
 print('Voce escolheste {}' .format(itens[jogador]))
+print('-=' * 11)
+
+if computador == 0:
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('VOCE VENCEU')
+    elif jogador == 2:
+        print('EU VENCE')
+    else:
+        print('Jogada invalida!')
+elif computador == 1:
+    if jogador == 0:
+        print('EU VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('VOCE VENCEU')
+    else:
+        print('Jogada invalida!')
+elif computador == 2:
+    if jogador == 0:
+        print('VOCE VENCEU')
+    elif jogador == 1:
+        print('EU VENCE')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('Jogada invalida!')
