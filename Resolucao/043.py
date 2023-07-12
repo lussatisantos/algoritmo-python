22b48f53eee23d04e9c9f913815e6d8f8172eac5
Lussati Santos <lussatisantos10@gmail.com>
2023-07-12 23:59:03 +0100

    base

--- Resolucao/043.py
+++ Resolucao/043.py
@@ -1,5 +1,13 @@
 from random import randint
 itens = ('Pedra', 'Papel', 'Tesoura')
 
+print(''' Suas opcoes:
+[0] Pedra
+[1] Papel
+[2] Tesoura
+''')
+jogador = int(input('Qual sera a sua jogada: '))
 computador = randint(0, 2)
+
 print('O computador escolheu {}' .format(itens[computador]))
+print('Voce escolheste {}' .format(itens[jogador]))
