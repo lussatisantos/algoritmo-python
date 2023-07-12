588fc4cad439484fd23653f3a9528850e26a9724
Lussati Santos <lussatisantos10@gmail.com>
2023-07-12 23:27:40 +0100

    Create 042.py

---
+++ Resolucao/042.py
@@ -0,0 +1,11 @@
+pago = float(input('Preco das compras: AOA '))
+
+print('''Qual e a forma de pagamento:
+[1] Dinheiro/Cheque
+[2] Cartao
+[3] 2x no Cartao
+[4] 3x ou mais no Cartao
+''') 
+
+receb = int(input('Qual opcao: '))
+
