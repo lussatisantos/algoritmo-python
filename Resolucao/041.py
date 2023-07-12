3fe75964f49962bee214018762ea0904d3b64305
Lussati Santos <lussatisantos10@gmail.com>
2023-07-12 22:48:07 +0100

    sobrepeso

--- Resolucao/041.py
+++ Resolucao/041.py
@@ -5,7 +5,10 @@ imc = peso / (altura ** 2)
 
 if imc < 18.5:
     print('O seu IMC e {}' .format(imc))
-    print('Voce esta abaixo do peso')
+    print('Voce esta ABAIXO DO PESO')
 elif imc <= 25:
     print('O seu IMC e {}' .format(imc))
     print('PARABENS, voce tem o PESO IDEAL')
+elif imc <= 30:
+    print('O seu IMC e {}' .format(imc))
+    print('Voce esta com SOBREPESO, aproveite fazer EXERCICIOS')
