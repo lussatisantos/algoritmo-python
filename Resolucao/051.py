f021314897800e0eaa1178406e7af9569465a546
Lussati Santos <lussatisantos10@gmail.com>
2023-07-13 15:19:12 +0100

    finished

--- Resolucao/051.py
+++ Resolucao/051.py
@@ -7,4 +7,9 @@ inverso = ''
 
 for letra in range(len(junto) -1, -1, -1):
      inverso += junto[letra]
-print(inverso)
+print('O inverso de {} sera {}'.format(junto, inverso))
+
+if inverso == junto:
+     print('Temos um PALINDROMO')
+else:
+     print('A frase digitada nao e um PALINDROMO')
