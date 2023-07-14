fcefe29deb78af5b5f11181b5096ad900d37e599
Lussati Santos <lussatisantos10@gmail.com>
2023-07-14 18:05:55 +0100

    finished

--- Resolucao/053.py
+++ Resolucao/053.py
@@ -1,6 +1,15 @@
 maior = 0
 menor = 0
+for i in range(1,6):
+    n = float(input('Quanto pesa a {} pessoa: ' .format(i)))
+    if i == 1:
+        maior = n
+        menor = n
+    else:
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
 
-for i in range(0,5):
-    n = float(input('Quantos pesos (km) tens? '))
-    
+print('O maior peso foi de {} kg' .format(maior))
+print('O menor peso foi de {} kg' .format(menor))
