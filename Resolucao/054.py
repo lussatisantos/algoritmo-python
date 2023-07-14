66129925480b920b378ad65c5e3d254387060f53
Lussati Santos <lussatisantos10@gmail.com>
2023-07-14 18:34:07 +0100

    finished

--- Resolucao/054.py
+++ Resolucao/054.py
@@ -2,6 +2,7 @@ soma_idade = 0
 media = 0
 maior_homem = 0
 velho = 0
+mulher = 0
 
 for grupo in range (1, 5):
     print('------ {} PESSOA ------' .format(grupo))
@@ -15,11 +16,14 @@ for grupo in range (1, 5):
         maior_homem = idade
         velho = nome
 
-if sexo in 'Mm' and idade > maior_homem:
-    maior_homem = idade
-    velho = nome
+    if sexo in 'Mm' and idade > maior_homem:
+        maior_homem = idade
+        velho = nome
+    if sexo in 'Ff' and idade > 20:
+        mulher += 1
 
 media = soma_idade / 4
 
 print('A media de idade do grupo e de {} anos' .format(media))
 print('O homem mais velho tem {} anos de nome {}' .format(maior_homem, velho))
+print('Ao todo sao {} mulheres com menos de 20 anos' .format(mulher))
