81171fd39025499232589056cd8f5e0c6acc3c3f
Lussati Santos <lussatisantos10@gmail.com>
2023-07-14 18:15:10 +0100

    Create 054.py

---
+++ Resolucao/054.py
@@ -0,0 +1,6 @@
+for grupo in range (1, 5):
+    print('------ {} PESSOA ------' .format(grupo))
+
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: '))
