9af92e042ebce5b8f4590f275b2ba26633a09090
Lussati Santos <lussatisantos10@gmail.com>
2023-07-15 13:41:46 +0100

    finished novo numero

--- Resolucao/057.py
+++ Resolucao/057.py
@@ -18,15 +18,15 @@ while opcao != 5:
 
     if opcao == 1:
         soma = numero1 + numero2
-        sleep(1)
         print ('A soma de {} e {} e igual a {}' .format(numero1, numero2, soma))
     elif opcao == 2:
         mult = numero1 * numero2
-        sleep(1)
         print('A multiplicacao de {} e {} e igual a {}' .format(numero1, numero2, mult))
     elif opcao == 3:
         if numero1 > numero2:
-            sleep(1)
             print('Entre {} e {}, o MAIOR e {}' .format(numero1, numero2, numero1))
         else:
             print('Entre {} e {}, o MAIOR e {}' .format(numero1, numero2, numero2))
+    elif opcao == 4:
+        numero1 = int(input('Digite um numero: '))
+        numero2 = int(input('Digite outro numero: '))
