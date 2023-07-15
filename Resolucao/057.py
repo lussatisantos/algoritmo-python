075070cf9ff6be1d03bfaef33e05e92c5cb47794
Lussati Santos <lussatisantos10@gmail.com>
2023-07-15 13:30:55 +0100

    soma finished

--- Resolucao/057.py
+++ Resolucao/057.py
@@ -1,3 +1,4 @@
+from time import sleep
 opcao = 0
 
 numero1 = int(input('Digite um numero: '))
@@ -6,15 +7,17 @@ numero2 = int(input('Digite outro numero: '))
 while opcao != 5:
     print ('''
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-        [ 1 ] Somar
-        [ 2 ] Multiplicar
-        [ 3 ] Maior
-        [ 4 ] Novos numeros
-        [ 5 ] Terminar o programa
+ [ 1 ] Somar
+ [ 2 ] Multiplicar
+ [ 3 ] Maior
+ [ 4 ] Novos numeros
+ [ 5 ] Terminar o programa
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
         ''')
     opcao = int(input('O que desejas: '))
 
     if opcao == 1:
         soma = numero1 + numero2
-        print
+        sleep(1)
+        print ('A soma de {} e {} e igual a {}' .format(numero1, numero2, soma))
+        
