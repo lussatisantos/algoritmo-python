157dfb9ab82e5efb2ebe7f380635f5900e82e748
Lussati Santos <lussatisantos10@gmail.com>
2023-07-15 12:35:01 +0100

    exercicio de menu

---
+++ Resolucao/057.py
@@ -0,0 +1,18 @@
+opcao = 0
+
+numero1 = int(input('Digite um numero: '))
+numero2 = int(input('Digite outro numero: '))
+
+while opcao != 5:
+    print ('''
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        [ 1 ] Somar
+        [ 2 ] Multiplicar
+        [ 3 ] Maior
+        [ 4 ] Novos numeros
+        [ 5 ] Terminar o programa
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        ''')
+    opcao = int(input('O que desejas: '))
+
+    
