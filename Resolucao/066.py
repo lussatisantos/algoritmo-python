452a0dfcb4a6e06876912d3d76e6a2fd14a44e61
Lussati Santos <lussatisantos10@gmail.com>
2023-07-16 14:52:36 +0100

    Update 066.py

--- Resolucao/066.py
+++ Resolucao/066.py
@@ -4,7 +4,7 @@ while True:
     jogador = int(input('Diga um valor: '))
     computador = randint(0, 10)
     total = jogador + computador
-    tipo = ''
-    while tipo not in 'PpIi':
-        tipo = str(input('Par ou Impar? [P/I] '))
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Impar? [P/I] ')).strip().upper()[0]
     print(f'Voce jogou {jogador} e o computador {computador}. Total de {total}')
