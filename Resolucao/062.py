1319efb08bceed3b59e03f559b1298f282060551
Lussati Santos <lussatisantos10@gmail.com>
2023-07-16 12:57:08 +0100

    finished

--- Resolucao/062.py
+++ Resolucao/062.py
@@ -1,10 +1,17 @@
-s = tot = 0
+s = tot = maior = menor = 0
 leitura = 'S'
 
 while leitura in 'Ss' and leitura not in 'Nn':
     num = int(input('Digite um numero: '))
+    menor = num
     leitura = str(input('Quer continuar? [S/N] '))
     s += 1
     tot += num
+
+    if menor > maior:
+        maior = menor
+    else:
+        menor = num
 media = tot / s
 print('Voce digitou {} numeros e a media foi {}' .format(s, media))
+print('O maior valor foi {} e o menor valor foi {}' .format(maior, menor))
