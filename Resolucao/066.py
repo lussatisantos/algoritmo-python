99671f2bb91200f8bda5d4894008708fc85414e5
Lussati Santos <lussatisantos10@gmail.com>
2023-07-16 15:00:23 +0100

    finished

--- Resolucao/066.py
+++ Resolucao/066.py
@@ -1,5 +1,5 @@
 from random import randint
-
+v = 0
 while True:
     jogador = int(input('Diga um valor: '))
     computador = randint(0, 10)
@@ -8,3 +8,20 @@ while True:
     while tipo not in 'PI':
         tipo = str(input('Par ou Impar? [P/I] ')).strip().upper()[0]
     print(f'Voce jogou {jogador} e o computador {computador}. Total de {total}')
+
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Voce VENCEU')
+            v += 1
+        else:
+            print('Voce PERDEU')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Voce VENCEU')
+            v += 1
+        else:
+            print('Voce PERDEU')
+            break
+    print('Vamos jogar novamente...')
+print(f'GAME OVER! Voce venceu {v} vezes')
