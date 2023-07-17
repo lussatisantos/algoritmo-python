4411eab118059333a0713ff621d9978e723bbb02
Lussati Santos <lussatisantos10@gmail.com>
2023-07-17 10:35:10 +0100

    Update 067.py

--- Resolucao/067.py
+++ Resolucao/067.py
@@ -1,4 +1,16 @@
+total = totmil = 0
 while True:
     nome = str(input('Digite o produto: ')).strip()
     preco = float(input('Digite o preco: '))
-    fim = str(input('Quer continuar? [S/N] '))
+    total += preco
+    if preco > 10000:
+        totmil += 1
+    s = ' '
+    while s not in 'SN':
+        s = str(input('Quer continuar? [S/N] ')).strip().upper()
+
+    if s == 'N':
+        break
+print('{:-^40}' .format(' FIM DO PROGRAMA '))
+print(f'O total da compra foi {total:10.2f}')
+print(f'Temos {totmil} produtos a custar mais de 10k')
