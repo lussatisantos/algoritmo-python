e0b803c884b08b232ac51ff59a4daad8aecb2a45
Lussati Santos <lussatisantos10@gmail.com>
2023-07-18 14:36:09 +0100

    finished

--- Resolucao/068.py
+++ Resolucao/068.py
@@ -1,9 +1,12 @@
-lista = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezanove', 'vinte')
+while True:
+    lista = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezanove', 'vinte')
 
-num = int(input('Digite um numero: '))
+    num = int(input('Digite um numero: '))
 
-if num < 0:
-    print('Numero invalido, tente novamente entre 0 a 20')
-elif num > 20:
-    print('Numero invalido, tente novamente entre 0 a 20')
-    
+    if num < 0:
+        print('Numero invalido, tente novamente entre 0 a 20')
+    elif num > 20:
+        print('Numero invalido, tente novamente entre 0 a 20')
+    else:
+        print(f'Numero {num} por extenso e {lista[num]}')
+        break
