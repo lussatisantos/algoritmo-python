27f61405d0dfdd5c820051ac27cbc345a7d49135
Lussati Santos <lussatisantos10@gmail.com>
2023-07-18 15:39:51 +0100

    finished

--- Resolucao/071.py
+++ Resolucao/071.py
@@ -6,7 +6,10 @@ num = (int(input('Digite um numero: ')),
 print(f'Voce digitou: {num}')
 
 print(f'O numero 9 apareceu {num.count(9)} vezes')
-print(f'O numero 3 apareceu na {num.index(3) + 1} posicao')
+if 3 in num:
+    print(f'O numero 3 apareceu na {num.index(3) + 1} posicao')
+else:
+    print('O numero 3 nao foi digitadi em nenhuma posicao')
 print('Os numeros pares digitados foram ', end='')
 
 for i in num:
