0c85c5712151cbee100f2e8cf12ba94ce828b167
Lussati Santos <lussatisantos10@gmail.com>
2023-07-18 15:08:16 +0100

    finished

---
+++ Resolucao/069.py
@@ -0,0 +1,9 @@
+tabela = ('Botafogo', 'Flamengo', 'Gremio', 'Sao Paulo', 'Fluminense', 'Palmeiras', 'Bragantino', 'Atletico Paranaense', 'Fortaleza', 'Cruzeiro', 'Internacional', 'Atletico MG', 'Cuiaba', 'Santos', 'Corinthians', 'Bahia', 'Goias', 'Coritiba', 'Vasco da Gama', 'America MG')
+
+print(f'Equipas do brasileira {tabela}')
+print('-='*20)
+print(f'Os 5 primeiros classificaos da tabela sao {tabela[:5]}')
+print('-='*20)
+print(f'Os ultimos 4 classicados da tabela sao {tabela[-4:]}')
+print('-='*20)
+print(f'De ordem alfabetica a tabela sera {sorted(tabela)}')
