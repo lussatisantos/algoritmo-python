03bc1a1d740ca5c794c125b12cdec4dcf9419e76
Lussati Santos <lussatisantos10@gmail.com>
2023-07-18 14:26:25 +0100

    Update 068.py

---
+++ Resolucao/068.py
@@ -0,0 +1,4 @@
+lista = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezanove', 'vinte')
+
+num = int(input('Digite um numero: '))
+
