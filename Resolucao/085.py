66e4d0337b607065cac65a9883dc20d3df523e14
Lussati Santos <lussatisantos10@gmail.com>
2023-07-20 00:40:10 +0100

    finished

--- Resolucao/085.py
+++ Resolucao/085.py
@@ -13,3 +13,12 @@ print(f'{"No.":<4}{"NOME":10}{"MEDIA":>8}')
 print('-='*25)
 for i, a in enumerate(ficha):
     print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-'*35)
+    opc =int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+    if opc == 999:
+        print('FINALIZANDO...')
+        break
+    if opc <= len(ficha) -1:
+        print(f'Notas de {ficha[opc][0]} sao {ficha[opc][1]}')
+print('<<< VOLTE SEMPRE >>>')
