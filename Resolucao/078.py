15cef524e7420a2661a8296ccf8be90c9bb46a9f
Lussati Santos <lussatisantos10@gmail.com>
2023-07-19 17:23:25 +0100

    finished

--- Resolucao/078.py
+++ Resolucao/078.py
@@ -8,14 +8,14 @@ while True:
     if num not in todos:
         todos.append(num)
     if num % 2 == 0:
-        pares.apped(num)
+        pares.append(num)
     else:
         impares.append(num)
 
-    opcao = 'S'
+    opcao = str(input('Quer continuar? [S/N]: '))
 
     if opcao in 'Nn':
         break
 print(f'A sua lista sera {todos}')
 print(f'Os valores pares da sua lista sao {pares}')
-print(f'Os valores impares da sua lita sao {impares}')
+print(f'Os valores impares da sua lista sao {impares}')
