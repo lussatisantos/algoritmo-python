cca4e7fc6a1f833992ba0f9affe617c796dbdc0d
Lussati Santos <lussatisantos10@gmail.com>
2023-07-20 00:33:46 +0100

    Update 085.py

--- Resolucao/085.py
+++ Resolucao/085.py
@@ -1,9 +1,15 @@
 ficha = list()
-
-nome = str(input('Nome: '))
-nota1 = float(input('Nota1: '))
-nota2 = float(input('Nota2: '))
-media = (nota1 + nota2) / 2
-ficha.append([nome, [nota1, nota2], media])
-
-print(ficha)
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota1: '))
+    nota2 = float(input('Nota2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('-='*30)
+print(f'{"No.":<4}{"NOME":10}{"MEDIA":>8}')
+print('-='*25)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
