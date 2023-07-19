faf8b1bbfc24650d3a2267213a0d02b78711a4e7
Lussati Santos <lussatisantos10@gmail.com>
2023-07-19 22:49:19 +0100

    pesos

--- Resolucao/080.py
+++ Resolucao/080.py
@@ -1,13 +1,24 @@
 temp = []
 princ = []
-
+maior = menor = 0
 while True:
     temp.append(str(input('Nome: ')))
     temp.append(float(input('Pesos: ')))
+    if len(princ) == 0:
+        maior = menor = temp[1]
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
     princ.append(temp[:])
     temp.clear()
 
     resp = str(input('Quer continuar? [S/N]: '))
     if resp in 'Nn':
         break
+print('=-'*20)
 print(f'Os dados foram {princ}')
+print(f'Ao todo, voce cadastrou {len(princ)} pessoas')
+print(f'O maior peso foi {maior}')
+print(f'O menor peso foi {menor}')
