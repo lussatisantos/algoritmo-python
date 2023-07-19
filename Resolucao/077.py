06be1d94be81205c663952752f520c5011f676fd
Lussati Santos <lussatisantos10@gmail.com>
2023-07-19 16:49:53 +0100

    Create 077.py

---
+++ Resolucao/077.py
@@ -0,0 +1,16 @@
+numeros = []
+
+while True:
+    n = int(input('Digite um numero: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Valor adicionado com sucesso!')
+    else:
+        print('Nao foi possivel adicionar este numero')
+    
+    opcao = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+
+    if opcao == 'N':
+        break
+
+print(f'A lista contem {len(numeros)} elementos')
