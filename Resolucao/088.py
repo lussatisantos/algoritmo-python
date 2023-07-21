9f6429400426b65d26157b42a943cfb3823e7532
Lussati Santos <lussatisantos10@gmail.com>
2023-07-21 15:44:51 +0100

    Update 088.py

--- Resolucao/088.py
+++ Resolucao/088.py
@@ -9,7 +9,15 @@ trabalho['carteira'] = int(input('Digite o numero da carteira [0 Nao tem]: '))
 
 print('-='*25)
 
+print(f'O seu nome e {trabalho["nome"]}')
+print(f'Voce tem actualmente {trabalho["ano"]} anos de idade')
+
 if trabalho['carteira'] == 0:
-    print(f'O seu nome e {trabalho["nome"]}')
-    print(f'Voce tem actualmente {trabalho["ano"]} anos de idade')
     print(f'Nao possui uma Carteira Profissional')
+else:
+    trabalho['contratacao'] = int(input('Ano de contratacao: '))
+    trabalho['salario'] = float(input('Salario (AOA): '))
+
+    print(f'Foi contratado em {trabalho["contratacao"]}')
+    print(f'Tem um salario de {trabalho["salario"]}')
+    
