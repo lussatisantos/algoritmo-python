da1f8cb8ad0c4d97e08a6859950bf7c67e6f528b
Lussati Santos <lussatisantos10@gmail.com>
2023-07-21 15:38:54 +0100

    Update 088.py

--- Resolucao/088.py
+++ Resolucao/088.py
@@ -5,4 +5,11 @@ trabalho = {}
 
 trabalho['nome'] = input('Digite o seu nome: ')
 trabalho['ano'] = hoje - int(input('Ano de nascimento: '))
-trabalho['carteira'] = int(input('Digite o numero da carteira [0 para encerrar]: '))
+trabalho['carteira'] = int(input('Digite o numero da carteira [0 Nao tem]: '))
+
+print('-='*25)
+
+if trabalho['carteira'] == 0:
+    print(f'O seu nome e {trabalho["nome"]}')
+    print(f'Voce tem actualmente {trabalho["ano"]} anos de idade')
+    print(f'Nao possui uma Carteira Profissional')
