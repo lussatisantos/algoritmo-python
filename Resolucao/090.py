599a2677ec7e92bd18dec10bcabd3d9c60bc445b
Lussati Santos <lussatisantos10@gmail.com>
2023-07-21 16:35:42 +0100

    Update 090.py

--- Resolucao/090.py
+++ Resolucao/090.py
@@ -1,16 +1,19 @@
 galera = []
 pessoa = {}
+soma = media = 0
 while True:
     pessoa.clear()
     pessoa['nome'] = input('Nome: ')
     while True:
         pessoa['sexo'] = input('Sexo [M/F]: ').upper()[0]
-        if pessoa['sexo'] in 'SM':
+        if pessoa['sexo'] in 'MF':
             break
         else:
             print('Erro, digite somente M ou F')
     pessoa['idade'] = int(input('Idade: '))
-    galera = pessoa[:]
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())  
+    
     while True:
         resp = input('Quer continuar? [S/N]: ').upper()[0]
         if resp in 'SN':
@@ -20,4 +23,6 @@ while True:
     if resp == 'N':
         break
 print('-='*25)
-print(galera)
+print(f'Ao todo temos {len(galera)} pessoas caadastradas')
+media = soma / len(galera)
+print(f'A media de idade e de {media:5.2f} anos')
