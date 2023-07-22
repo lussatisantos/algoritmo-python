24508728ae60bb01653d5b17ee231d93a8ba073a
Lussati Santos <lussatisantos10@gmail.com>
2023-07-22 16:49:33 +0100

    Update 096.py

--- Resolucao/096.py
+++ Resolucao/096.py
@@ -1,11 +1,16 @@
 from random import randint
+from time import sleep
 
 def sorteia(lista):
+    print('Sorteando 5 valores da lista: ', end='')
     for cont in range(0,5):
-        lista.append(randint(1, 10))
+        n = randint(1, 10)
+        lista.append(n)
+        print(f'{n} ', end='', flush=True)
+        sleep(0.4)
+    print('PRONTOS')
 
 #def somaPar():
 
 numeros = list()
 sorteia(numeros)
-print(numeros)
