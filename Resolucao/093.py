dbc9811e6f31076551e62b5ad07a867fd9dcbeba
Lussati Santos <lussatisantos10@gmail.com>
2023-07-22 15:47:29 +0100

    finished

---
+++ Resolucao/093.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~'*tam)
+    print(f'  {msg}')
+    print('~'*tam)
+
+
+
+escreva('Lussati')
+escreva('Lussati Santos')
+escreva('Curso de Python')
