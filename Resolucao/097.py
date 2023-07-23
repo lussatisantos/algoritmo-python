a7d89db6ad5ce3d23b00cfda764dd52d07094105
Lussati Santos <lussatisantos10@gmail.com>
2023-07-23 15:58:32 +0100

    finished

--- Resolucao/097.py
+++ Resolucao/097.py
@@ -1,7 +1,20 @@
-def factorial(a=0):
+def factorial(a=0, show=False):
+    """
+    -> Calcular o Factorial de um numero:
+    :params a: O numero a ser calculado
+    :params show: (Opcional) mostrar ou :nao a conta
+    :return: O valor do factorial de um :numero a
+    """
     cont = 1
     for i in range(a, 0, -1):
+        if show:
+            print(i, end='')
+            if i>1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
         cont *= i
     return cont
 
-print(factorial(5))
+print(factorial(5, show=True))
+help(factorial)
