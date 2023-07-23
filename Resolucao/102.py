661ba5dde4042121ec81ea3f51c89ea1c3723ff2
Lussati Santos <lussatisantos10@gmail.com>
2023-07-23 17:36:57 +0100

    Update 102.py

--- Resolucao/102.py
+++ Resolucao/102.py
@@ -1,17 +1,25 @@
+c = (
+    '\033[n', #0 - sem cor
+    '\033[0;30;41m', #1 - vermelho
+)
+
 def ajuda(com):
     help(com)
 
 def titulo(msg, cor=0):
-    tam =len(msg)
+    tam =len(msg) + 4
+    print(c[cor], end='')
     print('~'*tam)
-    print(msg)
+    print(f'{msg}')
     print('~'*tam)
+    print(c[cor], end='')
 
 comando = ''
 while True:
-    titulo('SISTEMA DE AJUDA PyHELP')
+    titulo('  SISTEMA DE AJUDA PyHELP', 1)
     comando = str(input('Funcao ou Biblioteca > '))
     if comando.upper() == 'FIM':
         break
     else:
         ajuda(comando)
+titulo('  ATE LOGO', 1)
