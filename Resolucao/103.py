4e749ac5eac1a8455817747e9a1492fb6c459ecb
Lussati Santos <lussatisantos10@gmail.com>
2023-07-24 15:04:44 +0100

    finished

---
+++ Resolucao/103.py
@@ -0,0 +1,5 @@
+from moeda import dobro, metade
+
+n = float(input('Digite um valor em AOA: '))
+print(f'O dobro de {n} Kz, e igual a {dobro(n)} Kz')
+print(f'A metade de {n} Kz, e igual a {metade(n)} Kz')
---
+++ Resolucao/moeda.py
@@ -0,0 +1,11 @@
+def aumentar(num):
+    return num+100
+
+def diminuir(num):
+    return num-100
+
+def dobro(num):
+    return num*2
+
+def metade(num):
+    return num/2
