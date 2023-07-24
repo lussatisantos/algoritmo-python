da21560a52d13fbf8f8fa35625d04e5baa79db2e
Lussati Santos <lussatisantos10@gmail.com>
2023-07-24 18:18:38 +0100

    Update app.py

--- Resolucao/ex108/app.py
+++ Resolucao/ex108/app.py
@@ -1,4 +1,15 @@
 from lib.interface import *
+from time import sleep
 
-
-resp = menu(['Criar arquivo', 'Cadastrar pessoas', 'Listar Pessoas', 'Sair do sistema'])
+while True:
+    resp = menu(['ver pessoas cadastradas', 'Cadastrar pessoas', 'Sair do sistema'])
+    if resp == 1:
+        cabecalho('Oopcao 1')
+    elif resp == 2:
+        cabecalho('Oopcao 2')
+    elif resp == 3:
+        cabecalho('Saindo do sistema... Ate logo')
+        break
+    else:
+        print('ERRO: digite uma opcao valida.')
+    sleep(2)
