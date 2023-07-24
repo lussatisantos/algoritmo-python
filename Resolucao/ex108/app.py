f5277789dd2b0ab821e889eb9f4a3834c1805710
Lussati Santos <lussatisantos10@gmail.com>
2023-07-24 18:10:03 +0100

    menu

--- Resolucao/ex108/app.py
+++ Resolucao/ex108/app.py
@@ -1,4 +1,4 @@
 from lib.interface import *
 
-cabecalho('SISTEMA ARQUIVO v1.0')
-menu(['opc01', 'opc02', 'opc03'])
+
+resp = menu(['Criar arquivo', 'Cadastrar pessoas', 'Listar Pessoas', 'Sair do sistema'])
--- Resolucao/ex108/lib/interface/__init__.py
+++ Resolucao/ex108/lib/interface/__init__.py
@@ -1,3 +1,16 @@
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('ERRO: por favor, digite um numero inteiro valido')
+            continue
+        except (KeyboardInterrupt):
+            print('Entrada de dados interrompida pelo usuario')
+            return 0
+        else:
+            return n
+        
 def linha(tam = 42):
     return '-' * tam
 
@@ -7,4 +20,12 @@ def cabecalho(txt):
     print(linha())
 
 def menu(lista):
-    print(lista)
+    cabecalho('MENU PRINCIPAL')
+    c = 1
+    for item in lista:
+        print(f'{c} - {item}')
+        c += 1
+    print(linha())
+    opc = leiaInt('Sua opcao: ')
+    return opc
+    
