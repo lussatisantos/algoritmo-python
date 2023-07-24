cbf914a2468f82d4a20ca79451643ff7b957c190
Lussati Santos <lussatisantos10@gmail.com>
2023-07-24 18:03:49 +0100

    i

---
+++ Resolucao/ex108/app.py
@@ -0,0 +1,4 @@
+from lib.interface import *
+
+cabecalho('SISTEMA ARQUIVO v1.0')
+menu(['opc01', 'opc02', 'opc03'])
---
+++ Resolucao/ex108/lib/interface/__init__.py
@@ -0,0 +1,10 @@
+def linha(tam = 42):
+    return '-' * tam
+
+def cabecalho(txt):
+    print(linha())
+    print(txt.center(42))
+    print(linha())
+
+def menu(lista):
+    print(lista)
